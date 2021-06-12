9f0032b9d51eef623637164765bf647b76cacaa8
Muhammed Shareef t <65382035+srf529@users.noreply.github.com>
2021-06-12 13:40:32 +0530

    Add files via upload
    Flutter Layout challenge .

---
+++ main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Simple '),
+      ),
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 200,
+              color: Colors.yellow,
+              padding: EdgeInsets.only(top: 100),
+              child: Container(
+                height: 100,
+                width: 100,
+                color: Colors.green[700],
+              ),
+            ),
+            // Container(
+            //   width: 100,
+            //   height: 100,
+            //   color: Colors.yellowAccent,
+            // ),
+            Container(
+              width: 100,
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
